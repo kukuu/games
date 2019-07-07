656f4eabb28849c9f74203e3b3dba76d08e4c212
Alexander Adu-Sarkodie <aadusarkodie1@gmail.com>
2019-07-07 21:03:40 +0100

    Removed redundancies - app.js

--- rock-paper-scissors/script/app.js
+++ rock-paper-scissors/script/app.js
@@ -1,7 +1,9 @@
 //Author: Alexander Adu-Sarkodie
 //Vanilla JavaScript orchestrated: Rock, Paper , Scissors game
 
-//Caching DOM ELEMENTS to enhance Performance: re-usable core features/interactive elements:: DRY/SOLID
+//Caching DOM ELEMENTS -  to enhance Performance: 
+//re-usable core features/interactive elements:: 
+//DRY/SOLID
 
 //setting defaults - Initialisation
 let userScore = 0;
@@ -22,10 +24,7 @@ const scissors_div = document.getElementById('s');
 //Computer Choice
 function getComputerChoice(){
 		const choices = ['r', 'p', 's'];
-		//Build a random function and floor ot to get atomic numbers
-		//console.log(Math.random() * 3)
-		//Ensuring ATOMICITY
-		//console.log(Math.floor(Math.random() * 3));
+		//Ensuring ATOMICITY with floor()
 		const randomNumber = Math.floor(Math.random() * 3);
 		return choices[randomNumber];
 }
@@ -43,38 +42,32 @@ function convertToWord(letter){
 //WIN
 function win(userChoice, computerChoice){
 	userScore++;
-	userScore_span.innerHTML = userScore;
-	computerScore_span.innerHTML = computerScore;
 	const smallUserWord = "user".fontsize(3).sup();
 	const smallCompWord = "comp".fontsize(3).sup();
 	const userChoice_div = document.getElementById(userChoice);
+	userScore_span.innerHTML = userScore;
+	computerScore_span.innerHTML = computerScore;
 //Refactored to use template strings from es6
 	result_p.innerHTML = `${convertToWord(userChoice)}${smallUserWord} beats   ${convertToWord(computerChoice)}${smallCompWord}.  You win (==)`;
-// Enhancing user experience with glow effect
+//Enhancing user experience with glow effect: refactored
 	//document.getElementById(userChoice).classList.add('green-glow');
 	userChoice_div.classList.add('green-glow');
 	//Adding set timeout to remove the glow border. Avoid persistence
-	//setTimeout(function(){console.log("Hello")}, 1000);
-	//setTimeout(function(){document.getElementById(userChoice).classList.remove('green-glow')}, 1000);
-	//setTimeout(function(){userChoice_div.classList.remove('green-glow')}, 1000);
-	setTimeout(() => userChoice_div.classList.remove('green-glow'), 1000);
+	setTimeout(() => userChoice_div.classList.remove('green-glow'), 300);
 }
 
 
 //LOSE
 function lose(userChoice, computerChoice){
 	computerScore++;
-	userScore_span.innerHTML = userScore;
-	computerScore_span.innerHTML = computerScore;
 	const smallUserWord = "user".fontsize(3).sup();
 	const smallCompWord = "comp".fontsize(3).sup();
 	const userChoice_div = document.getElementById(userChoice);
+	userScore_span.innerHTML = userScore;
+	computerScore_span.innerHTML = computerScore;
 	result_p.innerHTML = `${convertToWord(userChoice)}${smallUserWord} loses to   ${convertToWord(computerChoice)}${smallCompWord}. You lost...==)`;
-	//refactored
-	//document.getElementById(userChoice).classList.add('green-glow');
 	userChoice_div.classList.add('red-glow');
-	//setTimeout(function(){userChoice_div.classList.remove('red-glow')}, 300);
-	setTimeout(() => userChoice_div.classList.remove('red-glow'), 1000);
+	setTimeout(() => userChoice_div.classList.remove('red-glow'), 300);
 }
 
 
@@ -86,8 +79,7 @@ function draw(userChoice, computerChoice){
 	const userChoice_div = document.getElementById(userChoice);
 	result_p.innerHTML = `${convertToWord(userChoice)}${smallUserWord} equals  ${convertToWord(computerChoice)}${smallCompWord}. Its a draw!!`;
 	userChoice_div.classList.add('gray-glow');
-	//setTimeout(function(){userChoice_div.classList.remove('gray-glow')}, 300);
-	setTimeout(() => userChoice_div.classList.remove('gray-glow'), 1000);
+	setTimeout(() => userChoice_div.classList.remove('gray-glow'), 300);
 }
 
 
@@ -99,7 +91,7 @@ function game(userChoice){
 	const computerChoice = getComputerChoice();
 
 	switch(userChoice + computerChoice){
-		//combinations of user  WINS - ALGORITHM MATRIX
+		//  ALGORITHM MATRIX - combinations of user  WINS
 		case "rs":
 		case "pr":
 		case "sp":
@@ -107,7 +99,7 @@ function game(userChoice){
 		break;
 
 		//USER LOSES(computer wins) - note opposites of USER combinations
-		//case values registered here are  listed for each iterative option above - ALGORITHM MATRIX
+		//ALGORITHM MATRIX
 		case "rp":
 		case "ps":
 		case "sr":
@@ -118,7 +110,6 @@ function game(userChoice){
 		case "rr":
 		case "pp":
 		case "ss":
-		//console.log("It's a DRAW.")
 		draw(userChoice, computerChoice);
 		break;
 	}
