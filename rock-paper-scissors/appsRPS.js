37ef1bb4336aff22ce6c1ce07572650269e496d9
Alexander Adu-Sarkodie <aadusarkodie1@gmail.com>
2019-07-15 15:53:22 +0100

    Upload system under test - SUT

---
+++ rock-paper-scissors/appsRPS.js
@@ -0,0 +1,12 @@
+//Rock, Paper Scissors
+module.exports = {
+	win: function(){
+		return 'rs, pr or sp';
+	},
+	lose: function(){
+		return 'rp, ps or sr';
+	},
+	draw: function(){
+		return 'rr, pp or ss';
+	}
+}
