a8eeca165f9ee2f30f91180551126882f9c4caf8
Alexander Adu-Sarkodie <aadusarkodie1@gmail.com>
2019-07-28 19:46:38 +0100

    Update SUT - anagram.js

--- algorithms/anagram.js
+++ algorithms/anagram.js
@@ -4,12 +4,27 @@
 //Create a function that checks for this.
 
 
-describe("Anagrams", () => {
- 
- it("Should implement anagrams", () => {
-  assert.equal(anagrams("hello world", "world hello"), true);
-  assert.equal(anagrams("hellow world", "hello there"), false);
-  assert.equal(anagrams("hellow world", "hello there!"), false);
- });
+ const charCount = string => {
+    const table = {};
 
-});
+    for (let char of string.replace(/\W/g, "").toLowerCase())
+        table[char] = table[char] + 1 || 1;
+
+    return table;
+};
+
+const anagrams = (stringA, stringB) => {
+    const charCountA = charCount(stringA);
+    const charCountB = charCount(stringB);
+
+    if (Object.keys(charCountA).length !== Object.keys(charCountB).length)
+        return false;
+
+    for (let char in charCountA)
+        if (charCountA[char] !== charCountB[char]) return false;
+
+    return true;
+};
+
+const _sort = string => string.replace(/\W/g, "").toLowerCase().split("").sort().join("");
+const _anagrams = (stringA, stringB) => _sort(stringA) === _sort(stringB);
