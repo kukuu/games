e9720a06b0cca94a9e6dc723ecd6984b5688a583
Alexander Adu-Sarkodie <aadusarkodie1@gmail.com>
2019-09-09 11:30:38 +0100

    Adding fibonacci sequences.js

---
+++ algorithms/fibonnaci.js
@@ -0,0 +1,23 @@
+// Please write a function which given an input of an integer N
+// returns an array containing N fibonacci numbers.
+// Assume that the input is always an integer and never less than 2.
+//
+// Examples:
+// 2 => [0,1]
+// 3 => [0,1,1]
+// 8 => [0,1,1,2,3,5,8,13]
+
+function fb(n) {
+  //[0,1, 1, 2, 3, 5, 8]
+
+  let nums = [0, 1];
+
+  for (let i = 2; i < n; i++) {
+    nums.push(nums[i - 2] + nums[i - 1]);
+  }
+  console.log(nums);
+  return nums[n - 1];
+}
+
+// linear
+//expo: n poer 2
