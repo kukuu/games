91a4239d41ce69f823a398beda4231f7823b1da2
Alexander Adu-Sarkodie <aadusarkodie1@gmail.com>
2019-10-01 06:39:39 +0100

    Update clone.js

--- algorithms/copy-clone/clone.js
+++ algorithms/copy-clone/clone.js
@@ -3,7 +3,10 @@
 
 //Pure javascript method to deep clone object
 
-//How to copy JavaScript object to new variable NOT by reference?
+//In shallow copy, only fields of primitive data type are copied while the objects references are not copied. 
+//Deep copy involves the copy of primitive data type as well as objet references.
+
+//How to copy JavaScript object to new variable NOT by reference(Shallow)?
 //Importance: You can keep your copy in case you lose pointer to your original reference
 
 //Clone
