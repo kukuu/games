9df8cb8328cf88f20ea592b0b4ef69f52cb5e28c
Alexander Adu-Sarkodie <aadusarkodie1@gmail.com>
2019-11-14 14:42:42 +0000

    Setting React Counter Component for tdd;

---
+++ algorithms/tdd-test-snippets/javascript/react/counter.js
@@ -0,0 +1,35 @@
+
+
+import React, { Component } from 'react';
+
+class App extends Component {
+
+	state = {
+		counter = 0;
+	}
+
+	increment = () => {
+
+		let { counter } = this.state;
+		
+		//initialise counter
+		counter += 1;
+
+		this.setState({
+			counter;
+		})
+	}
+
+
+	render(){
+		return(
+			<div>
+				<h1>Hi</h1>
+				<h2>Counter is : { counter }</h2>
+				<button onclick = { this.increment }>Click Me. I am a counter</button>
+			</div>
+		)
+	}
+}
+
+export default App;
