23d7ee79deda937f55e93fa06bf2bcf79db86905
Alexander Adu-Sarkodie <aadusarkodie1@gmail.com>
2019-11-14 17:22:06 +0000

    Updated test script
    1. app.test.js
    2. console-app.test.js - with history and checks.

--- algorithms/tdd-test-snippets/javascript/react/console-app.test.js
+++ algorithms/tdd-test-snippets/javascript/react/console-app.test.js
@@ -13,15 +13,26 @@ describe('<App />', () => {
 
 	it('it renders props correctly', () = {
 		let component = shallow(< App name ="app" />);
+		//console.log(component.props());
+		//console.log(component.instance());
+
 		expect(component.instance().props.name).toBe('app');
 	});
 
 	it('It updates the counter on button  click', () => {
 		let component = mount(<App />);
+		//console.log(component);
 		const button = component.find('button');
-		button.simulate('click'); 
-		button.simulate('click');
-		button.simulate('click');
+		//console.log(button);
+		//console.log(button.props());//shows onclick as props
+		button.simulate('click'); //1
+		button.simulate('click');//2
+		button.simulate('click');//3
+		//console.log(component.state());
+		
+		//button.props().onclick();//Replace simulatefor single click.
+		//expect(component.state().counter).toEqual(1);
+
 		expect(component.state().counter).toEqual(3);
 
 	});
