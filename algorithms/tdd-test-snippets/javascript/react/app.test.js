59ef5b32a0cc8841600fc85415a9990f0be9de04
Alexander Adu-Sarkodie <aadusarkodie1@gmail.com>
2019-11-14 15:21:17 +0000

    Using mount(), and simulate to trigger execution.
    1. Mount to access behaviour (method)
    2. Simulate to re-run the behaviour for assetion.

--- algorithms/tdd-test-snippets/javascript/react/app.test.js
+++ algorithms/tdd-test-snippets/javascript/react/app.test.js
@@ -1,17 +1,30 @@
-
 import APP from './app';
-import { shallow } from 'enzyme';
+import { mount, shallow } from 'enzyme';
 
 describe('it should render <App /> component', () = {
 	let component = shallow(< App/>);
 	expect(component).toHaveLength(1);
-})
+});
 
 describe('it should render props', () = {
 	let component = shallow(< App name ="app" />);
 	expect(component).toHaveLength(1);
-	
+	console.log(component.props());
 	console.log(component.instance());
 
 	expect(component.instance().props).toBe('app');
-})
+});
+
+describe('It updates the button on click', () => {
+	let component = mount(<App />);
+	//console.log(component);
+	const button = component.find('button');
+	//console.log(button);
+	//console.log(button.props());//shows onclick as props
+	button.simulate('click'); //1
+	button.simulate('click');//2
+	//console.log(component.state());
+
+	expect(component.state().counter).toEqual(2);
+
+});
