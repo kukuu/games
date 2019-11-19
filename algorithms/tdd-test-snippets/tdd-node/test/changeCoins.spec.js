2eeca0b6cd780ce6ac8f202d28a3474ef9d64aa3
Alexander Adu-Sarkodie <aadusarkodie1@gmail.com>
2019-11-19 22:46:40 +0000

    Updated ChangeCoins.js

--- algorithms/tdd-test-snippets/tdd-node/test/changeCoins.spec.js
+++ algorithms/tdd-test-snippets/tdd-node/test/changeCoins.spec.js
@@ -3,7 +3,7 @@ import changeCoins from '../src/changeCoins.js';
 
 describe('src/changeCoins.js', () => {
 
-     it('should add 100p, 50p and 50p to make 200p', () => {
+     it('should add 100p, 50p and 50p to make £2', () => {
         let result = changeCoins(100,50,50);
 
         let resultFInal = "£" + (result/100);
@@ -11,7 +11,7 @@ describe('src/changeCoins.js', () => {
         console.log(resultFInal)
     });
 
-     it('should add 100p, 50p, 50p and 3p to make 253p', () => {
+     it('should add 100p, 50p, 50p and 3p to make £2.53', () => {
         let result = changeCoins(100, 50, 50, 50, 3);
 
         let resultFInal = "£" + (result/100);
@@ -21,7 +21,7 @@ describe('src/changeCoins.js', () => {
     });
 
 
-     it('should  throw an exception when you add a 100p, 50p, 50p and 3p to make 253pc', () => {
+     it('should  throw an exception when you add a 100p, 50p, 50p and 3p to make $253', () => {
         let result = changeCoins(100, 50, 50, 50, 3);
 
         let resultFInal = "£" + (result);
