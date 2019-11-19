9f0ebd1acc321c2b9cff611c90d270e49bc9da9d
Alexander Adu-Sarkodie <aadusarkodie1@gmail.com>
2019-11-19 17:45:52 +0000

    Added spec script for changeCoins.js

---
+++ algorithms/tdd-test-snippets/tdd-node/test/changeCoins.spec.js
@@ -0,0 +1,33 @@
+import {expect} from 'chai';
+import changeCoins from '../src/changeCoins.js';
+
+describe('src/changeCoins.js', () => {
+
+     it('should add 100p, 50p and 50p to make 200p', () => {
+        let result = changeCoins(100,50,50);
+
+        let resultFInal = "£" + (result/100);
+        expect(resultFInal).to.equal("£2");
+        console.log(resultFInal)
+    });
+
+     it('should add 100p, 50p, 50p and 3p to make 253p', () => {
+        let result = changeCoins(100, 50, 50, 50, 3);
+
+        let resultFInal = "£" + (result/100);
+
+        expect(resultFInal).to.equal("£2.53");
+        console.log(resultFInal)
+    });
+
+
+     it('should  throw an exception when you add a 100p, 50p, 50p and 3p to make 253pc', () => {
+        let result = changeCoins(100, 50, 50, 50, 3);
+
+        let resultFInal = "£" + (result);
+
+        expect(resultFInal).to.equal("$253");
+        console.log(resultFInal)
+    });
+
+})
--- algorithms/tdd-test-snippets/tdd-node/test/sum.spec.js
+++ algorithms/tdd-test-snippets/tdd-node/test/sum.spec.js
@@ -10,33 +10,6 @@ describe('src/sum.js', () => {
         console.log(result)
     })
 
-     it('should add 100p, 50p and 50p to make 200p', () => {
-        let result = sum(100,50,50);
-
-        let resultFInal = "£" + (result/100);
-        expect(resultFInal).to.equal("£2");
-        console.log(resultFInal)
-    });
-
-     it('should add 100p, 50p, 50p and 3p to make 253p', () => {
-        let result = sum(100, 50, 50, 50, 3);
-
-        let resultFInal = "£" + (result/100);
-
-        expect(resultFInal).to.equal("£2.53");
-        console.log(resultFInal)
-    });
-
-
-     it('should  throw an exception when you add a 100p, 50p, 50p and 3p to make 253pc', () => {
-        let result = sum(100, 50, 50, 50, 3);
-
-        let resultFInal = "£" + (result);
-
-        expect(resultFInal).to.equal("$253");
-        console.log(resultFInal)
-    });
-
     it('should add 1, 2 and 3 to make six', () => {
         let result = sum(1,2,3);
 
