77104b2bf5eeab9ee48f628d934221ff9f45897b
Alexander Adu-Sarkodie <aadusarkodie1@gmail.com>
2020-02-19 16:11:35 +0000

    Re-visioning Unit tests with Node, Mocha and Chai.
    Note the iterations of the test script. Making it terse and removing DRY-ness
    1. app.test.js
    2. app-v1.test.js to app-v4.test.js

---
+++ algorithms/tdd-test-snippets/mocha-chai/node-js-chai-mocha/test/app-v4.test.js
@@ -0,0 +1,26 @@
+const assert = require('chai').assert;
+const app = require('../app');
+
+//Results
+sayHelloResults = app.sayHell0();
+addNumbersResults = app.addNumbers(5,5);
+
+
+describe('App', () => {
+    it('sayHello should return hello', () => {
+        assert.equal(sayHelloResults, 'hello');
+    });
+
+    it('sayHello should return type string', () => {
+        assert.typeOf(sayHelloResults, "string");
+    });
+
+    it('addNumber should be above 5', () => {
+        assert.isAbove(addNumbersResults, 5);
+    });
+
+    it('sayHello should return a number type', () => {
+        assert.typeOf(addNumbersResults, 'number');
+    });
+
+});
---
+++ algorithms/tdd-test-snippets/mocha-chai/node-js-chai-mocha/test/app.test.js
@@ -0,0 +1,31 @@
+const assert = require('chai').assert;
+const app = require('../app');
+
+//Results
+sayHelloResults = app.sayHell0();
+addNumbersResults = app.addNumbers(5,5);
+
+
+describe('App', () => {
+    
+    describe('sayHello', () => {
+        it('sayHello should return hello', () => {
+            assert.equal(sayHelloResults, 'hello');
+        });
+
+        it('sayHello should return type string', () => {
+            assert.typeOf(sayHelloResults, "string");
+        });
+    });
+ 
+    describe('addNumbers', () => {
+        it('addNumber should be above 5', () => {
+            assert.isAbove(addNumbersResults, 5);
+        });
+    
+        it('sayHello should return a number type', () => {
+            assert.typeOf(addNumbersResults, 'number');
+        });
+    });   
+
+});
